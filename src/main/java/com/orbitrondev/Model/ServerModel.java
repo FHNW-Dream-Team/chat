6fa9887fab6a62761a3a542ec95dcad6f128e338
Manuele Vaccari <manuele.vaccari@gmail.com>
2019-11-18 01:10:14 +0100

    Add model to save server connection in db

---
+++ src/main/java/com/orbitrondev/Model/ServerModel.java
@@ -0,0 +1,62 @@
+package com.orbitrondev.Model;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "server")
+public class ServerModel {
+
+    /** FIELDS ////////////////////////////////*/
+
+    @DatabaseField(id = true, generatedId = true)
+    private int id;
+
+    @DatabaseField
+    private String ip;
+
+    @DatabaseField
+    private int port;
+
+    @DatabaseField(defaultValue = "false")
+    private boolean defaultServer = false;
+
+    /** CONSTRUCTORS //////////////////////////*/
+
+    ServerModel() {
+        // For ORMLite
+        // all persisted classes must define a no-arg constructor
+        // with at least package visibility
+    }
+
+    public ServerModel(String ip, int port) {
+        this.ip = ip;
+        this.port = port;
+    }
+    public ServerModel(String ip, int port, boolean defaultServer) {
+        this.ip = ip;
+        this.port = port;
+        this.defaultServer = defaultServer;
+    }
+
+    /** METHODS ///////////////////////////////*/
+
+    public int getId() {
+        return id;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public boolean isDefaultServer() {
+        return defaultServer;
+    }
+
+    public void setDefaultServer(boolean defaultServer) {
+        this.defaultServer = defaultServer;
+    }
+}
