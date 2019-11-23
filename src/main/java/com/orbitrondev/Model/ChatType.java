37911dd416a81032b0de9b305045a2489203777e
Manuele Vaccari <manuele.vaccari@gmail.com>
2019-11-23 12:59:15 +0100

    Removed unnecessary files

---
+++ src/main/java/com/orbitrondev/Model/ChatType.java
@@ -0,0 +1,29 @@
+package com.orbitrondev.Model.Chat;
+
+/**
+ * An enum defining the different chat types.
+ *
+ * @author Manuele Vaccari
+ * @version %I%, %G%
+ * @since 0.0.1
+ */
+public enum ChatType {
+    DirectChat, PublicGroupChat, PrivateGroupChat;
+
+    @Override
+    public String toString() {
+        String type = "";
+        switch (this) {
+            case DirectChat:
+                type = "DirectChat";
+                break;
+            case PublicGroupChat:
+                type = "PublicGroupChat";
+                break;
+            case PrivateGroupChat:
+                type = "PrivateGroupChat";
+                break;
+        }
+        return type;
+    }
+}
