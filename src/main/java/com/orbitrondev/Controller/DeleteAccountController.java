cfd87481445b607b87d9931b05d1cd13333a89a9
Manuele Vaccari <manuele.vaccari@gmail.com>
2019-12-25 16:24:52 +0100

    Add "Delete Account" MVC

--- src/main/java/com/orbitrondev/Controller/DeleteAccountController.java
+++ src/main/java/com/orbitrondev/Controller/DeleteAccountController.java
@@ -1,11 +1,130 @@
 package com.orbitrondev.Controller;
 
 import com.orbitrondev.Abstract.Controller;
-import com.orbitrondev.Model.DeleteAccountModel;
+import com.orbitrondev.Model.*;
+import com.orbitrondev.View.DashboardView;
 import com.orbitrondev.View.DeleteAccountView;
+import com.orbitrondev.View.Helper;
+import com.orbitrondev.View.LoginView;
+import javafx.application.Platform;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DeleteAccountController extends Controller<DeleteAccountModel, DeleteAccountView> {
     protected DeleteAccountController(DeleteAccountModel model, DeleteAccountView view) {
         super(model, view);
+
+        // register ourselves to listen for button clicks
+        view.getBtnDelete().setOnAction(event -> clickOnDelete());
+
+        // register ourselves to listen for button clicks
+        view.getBtnCancel().setOnAction(event -> clickOnCancel());
+
+        // register ourselves to handle window-closing event
+        view.getStage().setOnCloseRequest(event -> Platform.exit());
+    }
+
+    public void disableAll() {
+        view.getBtnDelete().setDisable(true);
+        view.getBtnCancel().setDisable(true);
+    }
+
+    public void enableAll() {
+        view.getBtnDelete().setDisable(false);
+        view.getBtnCancel().setDisable(false);
+    }
+
+    public void setErrorMessage(String translatorKey) {
+        Platform.runLater(() -> {
+            if (view.getErrorMessage().getChildren().size() == 0) {
+                // Make window larger, so it doesn't become crammed, only if we haven't done so yet
+                view.getStage().setHeight(view.getStage().getHeight() + 30);
+            }
+            Text text = Helper.useText(translatorKey);
+            text.setFill(Color.RED);
+            view.getErrorMessage().getChildren().clear();
+            view.getErrorMessage().getChildren().addAll(text, Helper.useSpacer(20));
+        });
+    }
+
+    public void clickOnDelete() {
+        // Disable everything to prevent something while working on the data
+        disableAll();
+
+        MainModel mainModel;
+        if (serviceLocator.getModel() == null) {
+            mainModel = new MainModel();
+            serviceLocator.setModel(mainModel);
+        } else {
+            mainModel = serviceLocator.getModel();
+        }
+
+        // Connection would freeze window (and the animations) so do it in a different thread.
+        Runnable logoutTask = () -> {
+            boolean userLoggedOut = false;
+            try {
+                // Try to logout
+                userLoggedOut = serviceLocator.getBackend().sendLogout();
+            } catch (IOException e) {
+                // This exception contains ConnectException, which basically means, it couldn't connect to the server.
+                enableAll();
+                setErrorMessage("gui.deleteAccount.logoutFailed");
+            }
+
+            if (userLoggedOut) {
+                mainModel.setCurrentLogin(null);
+                Platform.runLater(() -> {
+                    // Open login window and close delete account window
+                    Stage appStage = new Stage();
+                    LoginsModel model = new LoginsModel();
+                    LoginView newView = new LoginView(appStage, model);
+                    new LoginController(model, newView);
+
+                    view.stop();
+                    view = null;
+                    newView.start();
+                });
+            } else {
+                enableAll();
+                setErrorMessage("gui.deleteAccount.logoutFailed");
+            }
+        };
+        Runnable deleteTask = () -> {
+            boolean userDeleted = false;
+            try {
+                // Try to delete the account
+                userDeleted = serviceLocator.getBackend().sendDeleteLogin(serviceLocator.getModel().getCurrentLogin().getToken());
+            } catch (IOException e) {
+                // This exception contains ConnectException, which basically means, it couldn't connect to the server.
+                enableAll();
+                setErrorMessage("gui.deleteAccount.deleteFailed");
+            }
+
+            if (userDeleted) {
+                // If deleted, try logging out now.
+                new Thread(logoutTask).start();
+            } else {
+                enableAll();
+                setErrorMessage("gui.deleteAccount.deleteFailed");
+            }
+        };
+        new Thread(deleteTask).start();
+    }
+
+    public void clickOnCancel() {
+        Platform.runLater(() -> {
+            // Open dashboard window and close delete account window
+            Stage appStage = new Stage();
+            DashboardModel model = new DashboardModel();
+            DashboardView newView = new DashboardView(appStage, model);
+            new DashboardController(model, newView);
+
+            view.stop();
+            view = null;
+            newView.start();
+        });
     }
 }
--- src/main/java/com/orbitrondev/View/DeleteAccountView.java
+++ src/main/java/com/orbitrondev/View/DeleteAccountView.java
@@ -1,17 +1,96 @@
 package com.orbitrondev.View;
 
+import com.jfoenix.controls.JFXButton;
 import com.orbitrondev.Abstract.View;
+import com.orbitrondev.Controller.I18nController;
 import com.orbitrondev.Model.DeleteAccountModel;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class DeleteAccountView extends View<DeleteAccountModel> {
+    private VBox errorMessage;
+    private JFXButton btnDelete;
+    private JFXButton btnCancel;
+
     public DeleteAccountView(Stage stage, DeleteAccountModel model) {
         super(stage, model);
+        stage.titleProperty().bind(I18nController.createStringBinding("gui.deleteAccount.title"));
+        stage.setWidth(300);
+        stage.setResizable(false);
     }
 
     @Override
     protected Scene create_GUI() {
-        return null;
+        // Create root
+        VBox root = new VBox();
+        root.getStyleClass().add("background-white");
+
+        // Create body
+        VBox body = new VBox();
+        body.getStyleClass().add("custom-container");
+
+        // Create error message container
+        errorMessage = new VBox();
+
+        // Create body
+        HBox btnRow = new HBox();
+        btnRow.setSpacing(4); // Otherwise the login and register are right beside each other
+
+        // Create button to register
+        btnDelete = Helper.usePrimaryButton("gui.deleteAccount.delete");
+
+        // Create button to change
+        btnCancel = Helper.useSecondaryButton("gui.deleteAccount.cancel");
+
+        // Add buttons to btnRow
+        btnRow.getChildren().addAll(
+            btnDelete,
+            Helper.useHorizontalSpacer(1),
+            btnCancel
+        );
+
+        // Add body content to body
+        body.getChildren().addAll(
+            errorMessage,
+            Helper.useSpacer(10),
+            Helper.useText("gui.deleteAccount.message", stage),
+            Helper.useSpacer(25),
+            btnRow
+        );
+
+        // Add body to root
+        root.getChildren().addAll(
+            Helper.useDefaultMenuBar(),
+            Helper.useNavBar("gui.changePassword.title"),
+            body
+        );
+
+        Scene scene = new Scene(root);
+        // https://stackoverflow.com/questions/29962395/how-to-write-a-keylistener-for-javafx
+        scene.setOnKeyPressed(event -> {
+            // Click the connect button by clicking ENTER
+            if (event.getCode() == KeyCode.ENTER) {
+                if (!btnDelete.isDisable()) {
+                    btnDelete.fire();
+                }
+            }
+        });
+        scene.getStylesheets().add(getClass().getResource("/css/app.css").toExternalForm());
+        return scene;
+    }
+
+    public VBox getErrorMessage() {
+        return errorMessage;
+    }
+
+    public JFXButton getBtnDelete() {
+        return btnDelete;
+    }
+
+    public JFXButton getBtnCancel() {
+        return btnCancel;
     }
 }
