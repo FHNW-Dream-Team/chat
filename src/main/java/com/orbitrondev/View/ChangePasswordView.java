30f8ce508c4004a126444823ea399d41272f2be6
Manuele Vaccari <manuele.vaccari@gmail.com>
2019-12-25 14:07:19 +0100

    Add "Change Password" MVC

--- src/main/java/com/orbitrondev/View/ChangePasswordView.java
+++ src/main/java/com/orbitrondev/View/ChangePasswordView.java
@@ -11,6 +11,8 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ChangePasswordView extends View<ChangePasswordModel> {
     private VBox errorMessage;
     private JFXPasswordField oldPassword;
