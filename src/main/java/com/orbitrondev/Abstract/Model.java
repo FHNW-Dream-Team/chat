fe04c922e279ee3fbbfa94385fe9f11e0e7ffd37
Manuele Vaccari <manuele.vaccari@gmail.com>
2019-12-27 03:23:52 +0100

    Add service locator to the models in general

--- src/main/java/com/orbitrondev/Abstract/Model.java
+++ src/main/java/com/orbitrondev/Abstract/Model.java
@@ -1,5 +1,7 @@
 package com.orbitrondev.Abstract;
 
+import com.orbitrondev.Controller.ServiceLocator;
+
 /**
  * Copyright 2015, FHNW, Prof. Dr. Brad Richards. All rights reserved. This code
  * is licensed under the terms of the BSD 3-clause license (see the file
@@ -8,7 +10,9 @@ package com.orbitrondev.Abstract;
  * @author Brad Richards
  */
 public abstract class Model {
+    protected ServiceLocator serviceLocator;
+
     protected Model() {
-        // Nothing to see here, move along
+        serviceLocator = ServiceLocator.getServiceLocator();
     }
 }
